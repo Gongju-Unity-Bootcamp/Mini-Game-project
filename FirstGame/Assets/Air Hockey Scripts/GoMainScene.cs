c5e4b248a802779a37529f90c6f914a0f95c33f9
HyeukJin <lytmmr@gmail.com>
2023-11-14 15:02:12 +0900

    Modify Repogitory Files

---
+++ FirstGame/Assets/Air Hockey Scripts/GoMainScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoMainScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(LoadMainScene);
+    }
+
+    public void LoadMainScene()
+    {
+        Score.player1Score = 0;
+        Score2.player2Score = 0;
+        SceneManager.LoadScene("MainScene");
+    }
+}
